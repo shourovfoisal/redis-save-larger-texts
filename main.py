6be86953ec419b4ffd0296680fed84bc786820bd
shourov.foisal <shourov.foisal@technonext.com>
2025-07-10 12:59:59 +0600

    fix general key name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from embeddings import televisions, fan
 
 try:
   r = redis.Redis(host='localhost', port=6379)
-  r.set('aes::textEmbeddingCache::televisions', televisions)
+  r.set('televisions', televisions)
 except Exception as e:
   print("Redis exception")
   print(e)
